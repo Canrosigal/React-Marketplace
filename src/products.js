226e439ba04f56b80b624ab60037fdff048fee4f
Canrosigal <canrosigal@gmail.com>
2024-05-02 16:05:03 +0700

    membuat-marketplace-sederhana

---
+++ src/products.js
@@ -0,0 +1,51 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Pull and Bear Jeans",
+    Desc: "Celana Panjang Pull and Bear skinny Jeans",
+    price: 190000,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Pull and Bear Jeans",
+    Desc: "Celana Panjang Pull and Bear skinny Jeans",
+    price: 195000,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Pull and Bear Jeans",
+    Desc: "Celana Panjang Pull and Bear skinny Jeans",
+    price: 175000,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Pull and Bear Jeans",
+    Desc: "Celana Panjang Pull and Bear skinny Jeans",
+    price: 167000,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Pull and Bear Jeans",
+    Desc: "Celana Panjang Pull and Bear skinny Jeans",
+    price: 180000,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Pull and Bear Jeans",
+    Desc: "Celana Panjang Pull and Bear skinny Jeans",
+    price: 190000,
+    productImage: product6,
+  },
+];
